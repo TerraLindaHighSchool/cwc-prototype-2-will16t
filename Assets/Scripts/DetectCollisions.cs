6132a28436265c2c404c43040d906ab47a142eb5
will16t <60672309+will16t@users.noreply.github.com>
2020-12-06 17:24:43 -0800

    MissingReferenceException

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -15,7 +15,7 @@ public class DetectCollisions : MonoBehaviour
     {
         
     }
-    void onTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
         Destroy(other.gameObject);
